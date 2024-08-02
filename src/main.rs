bd49ccc714140ad2b8f756c1e83d360580b3b293
Julio Teixeira <julioct.dev@gmail.com>
2024-08-02 05:20:53 -0300

    feat: create menu enum

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,77 @@ mod menu;
 mod user;
 
 use app::AppState;
+use user::User;
 
 fn main() {
     let mut app = AppState::new();
-
     let mut users = io::load_data();
-    println!("Users: {:#?}", &users);
-    app.get_user();
-    // app.set_user(&mut users[0]);
-    app.get_user();
 
     match menu::create_main_menu(&app) {
-        0 => println!("Selected 0"),
-        1 => println!("Selected 1"),
-        val => println!("Selected something else: {val}"),
+        0 => {
+            // Select User
+
+            // * SELECTING A USER
+            let selected_idx = menu::create_generic_menu(
+                "Choose an user",
+                &extract_names_from_users(&users, true),
+            );
+            if selected_idx == users.len() {
+                // Back chosen
+                println!("Back chosen");
+                return;
+            }
+            app.set_user(&mut users[selected_idx]);
+
+            // * USER MENU
+            let (selected_idx, back_idx) = menu::create_user_menu(&app);
+
+            if selected_idx == back_idx {
+                // Back chosen
+                println!("Back chosen");
+                return;
+            }
+            match selected_idx {
+                0 => {
+                    println!("List contacts")
+                }
+                1 => {
+                    println!("Edit User")
+                }
+                _ => unreachable!(),
+            }
+        }
+        1 => {
+            // Register an user
+            println!("Selected 1")
+        }
+        2 => {
+            println!("Good Bye!")
+        }
+        _ => unreachable!(),
     }
+}
+
+fn extract_names_from_users(users: &Vec<User>, put_back: bool) -> Vec<&str> {
+    let mut names: Vec<&str> = Vec::new();
+    for u in users {
+        names.push(&u.name);
+    }
+
+    if put_back == true {
+        names.push("<- Back");
+    }
+    names
+}
+
+fn selecting_a_user<'a>(app: &'a mut AppState<'a>, users: &'a mut Vec<User>) {
+    // * SELECTING A USER
+    let selected_idx =
+        menu::create_generic_menu("Choose an user", &extract_names_from_users(&users, true));
+    if selected_idx == users.len() {
+        // Back chosen
+        println!("Back chosen");
+        return;
+    }
+    app.set_user(&mut users[selected_idx]);
 }
--- src/menu.rs
+++ src/menu.rs
@@ -1,20 +1,58 @@
 use crate::app::AppState;
 use colored::Colorize;
-use dialoguer::{theme::ColorfulTheme, Input, Select};
+use dialoguer::{theme::ColorfulTheme, Select};
+use std::process::Command;
 
-pub fn create_main_menu(app: &AppState) -> usize {
-    println!("################################");
-    println!("Selected: {}", app.get_user().green());
-    println!("################################\n");
+pub enum MenuState {
+    MainMenu,
+    SelectUserMenu,
+    RegisterUser,
+    ListContacts,
+    EditUserAndContacts,
+    Quit,
+}
 
-    let items = &["Select a user", "Register a user"];
+fn clear_prompt() {
+    #[cfg(target_os = "windows")]
+    Command::new("cls")
+        .status()
+        .expect("failed to execute process");
 
+    #[cfg(not(target_os = "windows"))]
+    Command::new("clear")
+        .status()
+        .expect("failed to execute process");
+}
+pub fn create_generic_menu(prompt: &str, items: &[&str]) -> usize {
     let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Choose an option")
+        .with_prompt(prompt)
         .default(0)
         .items(&items[..])
         .interact()
         .unwrap();
 
     selection
+}
+
+pub fn create_user_menu(app: &AppState) -> (usize, usize) {
+    show_selected_user_on_prompt(app);
+    let items = ["List contacts", "Edit User", "<- Back"];
+    let generic_menu = create_generic_menu(
+        "Choose an option related to the current selected user",
+        &items,
+    );
+    (generic_menu, items.len() - 1)
+}
+
+fn show_selected_user_on_prompt(app: &AppState) {
+    clear_prompt();
+    println!("################################");
+    println!("Selected: {}", app.get_user().green());
+    println!("################################\n");
+}
+
+pub fn create_main_menu(app: &AppState) -> usize {
+    show_selected_user_on_prompt(app);
+    let items = &["Select a user", "Register a user", "Quit"];
+    create_generic_menu("Choose an option", items)
 }
