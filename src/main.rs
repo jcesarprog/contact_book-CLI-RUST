a015dcc387f4830d390a1476a62f641bef0918d4
Julio Teixeira <julioct.dev@gmail.com>
2024-08-02 07:08:22 -0300

    dealing with borrow checker to figure out how to deal with menus and passing state references

--- src/main.rs
+++ src/main.rs
@@ -3,56 +3,89 @@ mod io;
 mod menu;
 mod user;
 
+use std::process::exit;
+
 use app::AppState;
+use menu::MenuState;
 use user::User;
 
 fn main() {
     let mut app = AppState::new();
     let mut users = io::load_data();
 
-    match menu::create_main_menu(&app) {
-        0 => {
-            // Select User
-
-            // * SELECTING A USER
-            let selected_idx = menu::create_generic_menu(
-                "Choose an user",
-                &extract_names_from_users(&users, true),
-            );
-            if selected_idx == users.len() {
-                // Back chosen
-                println!("Back chosen");
-                return;
-            }
-            app.set_user(&mut users[selected_idx]);
-
-            // * USER MENU
-            let (selected_idx, back_idx) = menu::create_user_menu(&app);
-
-            if selected_idx == back_idx {
-                // Back chosen
-                println!("Back chosen");
-                return;
-            }
-            match selected_idx {
-                0 => {
-                    println!("List contacts")
-                }
-                1 => {
-                    println!("Edit User")
-                }
-                _ => unreachable!(),
-            }
+    let mut menu_state = match menu::main_menu(&app) {
+        MenuState::UserSelectionMenu => menu::user_selection_menu(&mut users, &mut app),
+        // ! Need to implement this register_user function
+        MenuState::RegisterUser => register_user(&mut users, &mut app),
+        _ => {
+            println!("Good bye");
+            exit(0)
         }
-        1 => {
-            // Register an user
-            println!("Selected 1")
-        }
-        2 => {
-            println!("Good Bye!")
-        }
-        _ => unreachable!(),
+    };
+    {
+        match menu_state {
+            MenuState::UserMenu => menu::user_menu(&app),
+            MenuState::MainMenu => menu::main_menu(&app),
+            _ => unreachable!(),
+        };
     }
+
+    // while menu_state != MenuState::Quit {
+    //     menu_state = match menu_state {
+    //         MenuState::MainMenu => menu::main_menu(&app),
+    //         MenuState::UserMenu => menu::user_menu(&app),
+    //         MenuState::UserSelectionMenu => menu::user_selection_menu(&mut users, &mut app),
+    //         _ => MenuState::Quit,
+    //     };
+    // }
+    // ###############
+    // match menu::create_main_menu(&app) {
+    //     0 => {
+    //         // Select User
+
+    //         // * SELECTING A USER
+    //         let selected_idx = menu::create_generic_menu(
+    //             "Choose an user",
+    //             &extract_names_from_users(&users, true),
+    //         );
+    //         if selected_idx == users.len() {
+    //             // Back chosen
+    //             println!("Back chosen");
+    //             return;
+    //         }
+    //         app.set_user(&mut users[selected_idx]);
+
+    //         // * USER MENU
+    //         let (selected_idx, back_idx) = menu::create_user_menu(&app);
+
+    //         if selected_idx == back_idx {
+    //             // Back chosen
+    //             println!("Back chosen");
+    //             return;
+    //         }
+    //         match selected_idx {
+    //             0 => {
+    //                 println!("List contacts")
+    //             }
+    //             1 => {
+    //                 println!("Edit User")
+    //             }
+    //             _ => unreachable!(),
+    //         }
+    //     }
+    //     1 => {
+    //         // Register an user
+    //         println!("Selected 1")
+    //     }
+    //     2 => {
+    //         println!("Good Bye!")
+    //     }
+    //     _ => unreachable!(),
+    // }
+}
+
+fn register_user(users: &mut Vec<User>, app: &mut AppState) -> MenuState {
+    MenuState::UserMenu
 }
 
 fn extract_names_from_users(users: &Vec<User>, put_back: bool) -> Vec<&str> {
--- src/menu.rs
+++ src/menu.rs
@@ -1,11 +1,13 @@
-use crate::app::AppState;
+use crate::{app::AppState, user::User};
 use colored::Colorize;
 use dialoguer::{theme::ColorfulTheme, Select};
 use std::process::Command;
 
+#[derive(PartialEq)]
 pub enum MenuState {
     MainMenu,
-    SelectUserMenu,
+    UserSelectionMenu,
+    UserMenu,
     RegisterUser,
     ListContacts,
     EditUserAndContacts,
@@ -55,4 +57,69 @@ pub fn create_main_menu(app: &AppState) -> usize {
     show_selected_user_on_prompt(app);
     let items = &["Select a user", "Register a user", "Quit"];
     create_generic_menu("Choose an option", items)
+}
+
+pub fn main_menu(app: &AppState) -> MenuState {
+    show_selected_user_on_prompt(app);
+    let items = &["Select a user", "Register a user", "Quit"];
+    let selection = create_generic_menu("Choose an option", items);
+    match selection {
+        0 => MenuState::UserSelectionMenu,
+        1 => MenuState::RegisterUser,
+        _ => MenuState::Quit,
+    }
+}
+
+pub fn user_selection_menu<'a>(users: &'a mut Vec<User>, app: &'a mut AppState<'a>) -> MenuState {
+    // * SELECTING A USER
+    let selected_idx =
+        create_generic_menu("Choose an user", &extract_names_from_users(&users, true));
+    match selected_idx {
+        idx if idx == users.len() => {
+            // Back chosen
+            println!("Back chosen");
+            MenuState::MainMenu
+        }
+        idx => {
+            app.set_user(&mut users[idx]);
+            MenuState::UserMenu
+        }
+    }
+    // if selected_idx == users.len() {
+    //     // Back chosen
+    //     println!("Back chosen");
+    //     return MenuState::MainMenu;
+    // }
+
+    // app.set_user(&mut users[selected_idx]);
+    // MenuState::UserMenu
+}
+
+fn extract_names_from_users(users: &Vec<User>, put_back: bool) -> Vec<&str> {
+    let mut names: Vec<&str> = Vec::new();
+    for u in users {
+        names.push(&u.name);
+    }
+
+    if put_back == true {
+        names.push("<- Back");
+    }
+    names
+}
+
+pub fn user_menu(app: &AppState) -> MenuState {
+    show_selected_user_on_prompt(app);
+    let items = ["List contacts", "Edit User", "<- Back"];
+    let generic_menu = create_generic_menu(
+        "Choose an option related to the current selected user",
+        &items,
+    );
+    // (generic_menu, items.len() - 1);
+    // ()
+    match generic_menu {
+        0 => MenuState::ListContacts,
+        1 => MenuState::EditUserAndContacts,
+        2 => MenuState::UserSelectionMenu,
+        _ => unreachable!(),
+    }
 }
