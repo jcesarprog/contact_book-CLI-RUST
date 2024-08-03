4073073d839820801fca7e703f5b4dfdc944dfcb
Julio Teixeira <julioct.dev@gmail.com>
2024-08-03 07:40:32 -0300

    feat: deleted trash code on main, still need to solve borrowing there

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,8 @@ mod io;
 mod menu;
 mod user;
 
-use std::process::exit;
-
 use app::AppState;
 use menu::MenuState;
-use user::User;
 
 fn main() {
     let mut app = AppState::new();
@@ -19,13 +16,13 @@ fn main() {
             MenuState::MainMenu => menu::main_menu(&app),
             MenuState::UserMenu => menu::user_menu(&app),
             MenuState::UserSelectionMenu => {
-                // Limit the scope of the mutable borrow of `users` and `app`
+                // ! Need to Limit the scope of the mutable borrow of `users` and `app`
                 let next_state = menu::user_selection_menu(&mut users, &mut app);
                 next_state
             }
             MenuState::Quit => {
                 println!("Good Bye!");
-                exit(0);
+                std::process::exit(0);
             }
             _ => unreachable!(),
         };
