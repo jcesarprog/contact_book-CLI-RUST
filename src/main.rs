19973ba74bdf6609a0b35095d31752b6578f4960
Julio Teixeira <julioct.dev@gmail.com>
2024-08-03 06:50:52 -0300

    wip need to fix main loop error related to borrowing

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,20 @@ fn main() {
     let mut users = io::load_data();
 
     let mut menu_state = MenuState::MainMenu;
-    while menu_state != MenuState::Quit {
-        match menu_state {
-            MenuState::MainMenu => {
-                menu_state = menu::main_menu(&app);
-            }
-            MenuState::UserMenu => {
-                menu_state = menu::user_menu(&app);
-            }
+    loop {
+        menu_state = match menu_state {
+            MenuState::MainMenu => menu::main_menu(&app),
+            MenuState::UserMenu => menu::user_menu(&app),
             MenuState::UserSelectionMenu => {
                 // Limit the scope of the mutable borrow of `users` and `app`
                 let next_state = menu::user_selection_menu(&mut users, &mut app);
-                menu_state = next_state;
+                next_state
             }
             MenuState::Quit => {
                 println!("Good Bye!");
                 exit(0);
             }
             _ => unreachable!(),
-        }
+        };
     }
 }
